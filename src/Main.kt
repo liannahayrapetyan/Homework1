fcdb3c2618e7d3a7e3cab209c6efbd127d76b1dc
liannahayrapetyan <hyanlian032@gmail.com>
2024-09-22 09:25:46 +0400

    first homework, use Kotlin higher order function

--- src/Main.kt
+++ src/Main.kt
@@ -1,6 +1,6 @@
 // Homework 1
 
-import factory.ShapeFactory
+/*import factory.ShapeFactory
 
 fun main() {
     val factory = ShapeFactory()
@@ -11,11 +11,45 @@ fun main() {
     println("Circle area: ${circle.calculateArea()}")
     println("Rectangle area: ${rectangle.calculateArea()}")
     println("Square area: ${square.calculateArea()}")
-}
+}*/
 // https://www.w3resource.com/kotlin-exercises/class/kotlin-class-exercise-14.php#google_vignette
 
 
+
 // Homework 2
+
+// Higher order function
+/*
+fun calculate(a: Double, b: Double, operation:(Double, Double) -> Double): Double {
+    return operation(a, b)
+}
+
+fun main(args: Array<String>) {
+
+    print("Enter the first number: ")    // first number
+    val num1 = readLine()?.toDoubleOrNull()
+
+    print("Enter the second number: ")
+    val num2 = readLine()?.toDoubleOrNull() // second number
+
+    if (num1 == null || num2 == null) {
+        println("Enter valid numbers.")   // if numbers are valid
+        return
+    }
+
+
+    println("Sum is " + calculate(num1, num2) { a, b -> a + b })
+    println("Difference is " + calculate(num1, num2) { a, b -> a-b })
+    println("Product " + calculate(num1, num2) { a, b -> a*b })
+
+
+
+}
+
+*/
+
+
+// simple version
 /*
 fun calculate(a: Double, b: Double, operation: String): Double {
 
@@ -52,14 +86,69 @@ fun main() {
 */
 
 
-
-
-
-
-
 // Homework 3
+// real work
 /*
-(fun main() {
+fun main() {
+    // Create two-dimensional array
+    val array = arrayOf(
+        arrayOf(1, 2, 3, 4),
+        arrayOf(4, 5, 6, 5),
+        arrayOf(4, 8, 9, 2)
+    )
+
+    // Sums for diagonals
+    var primaryDiagonalSum = 0 // top-left to bottom-right
+    var secondaryDiagonalSum = 0 //top-right to bottom-left
+    val size = min(array.size, array[0].size)
+
+    // Print
+
+    print("Primary diagonal: ")
+    for (i in 0..<size) {
+        primaryDiagonalSum += array[i][i]
+        print("${array[i][i]} ${if (i < size - 1) " + " else " = "}")
+    }
+    println(primaryDiagonalSum)
+
+    print("Secondary diagonal: ")
+    for (i in 0..<size) {
+        secondaryDiagonalSum += array[i][size - 1 - i]
+        print("${array[i][size - 1 - i]} ${if (i < size - 1) " + " else " = "}")
+    }
+    println(secondaryDiagonalSum)
+}*/
+
+
+
+fun main() {
+    val matrix = arrayOf(
+        intArrayOf(1, 2, 1, 2),
+        intArrayOf(1, 2, 6, 5),
+        intArrayOf(4, 4, 4 ),
+        intArrayOf(1, 7, 3, 1)
+    )
+    val primaryDiagonalSum = calculatePrimaryDiagonalSum(matrix, { i, size -> matrix[i][i] }) // calculatePrimaryDiagonalSum(matrix) {i:Int, _: Int -> i
+    val secondaryDiagonalSum = calculatePrimaryDiagonalSum(matrix, { i, size -> matrix[i][size - 1 - i] })
+
+    println("Primary diagonal: $primaryDiagonalSum")
+    println("Secondary diagonal: $secondaryDiagonalSum")
+
+}
+
+fun calculatePrimaryDiagonalSum(matrix: Array<IntArray>, diaganal: (index: Int, size: Int) -> Int): Int {
+    val size = min(matrix.size, matrix.minOf{ it.size })//եթե անհամաչափ զանգված է, պետք է գտնել ամենափոքր size ուենցող տողը
+
+    var sum = 0
+
+    for (i in 0..<size) {
+        sum += diaganal(i, size)
+    }
+    return sum
+}
+
+/*
+fun main() {
     // Create two-dimensional array
     val array = arrayOf(
         arrayOf(1, 2, 3),
