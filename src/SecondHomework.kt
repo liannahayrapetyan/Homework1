0cd5fd4fe1b851223833ed1dda174d9a8029fb68
liannahayrapetyan <hyanlian032@gmail.com>
2024-10-13 10:12:06 +0400

    1. Given the two integer arrays (or list) that are sorted. Create a new array that contains all the elements, ensuring the result is also sorted (The complexity should be O(n)). a. Example: array1 = [1, 2, 3], array2 = [1, 3, 4], resultArray = [1, 1, 2, 3, 3, 4]
    2. Given a string that contains. Find the max path between two identical letters. If there is no such case return -1;
    a. Example: s = "abbbabac", result = 5 (there are 5 letters between the first a and the last a)
    b. Example  s = "abqcdefbghqi", result = 7 (there are 7 letters between the first q and the last q)
    c. Example  s = "abcdef", result = -1 (no identical letters found)

---
+++ src/SecondHomework.kt
@@ -0,0 +1,127 @@
+fun approach2(nums1: List<Int>, nums2: List<Int>): List<Int>{
+
+    var union = mutableListOf<Int>()
+
+    var i = 0
+    var j = 0
+
+    while(i < nums1.size && j < nums2.size){
+        when{
+            nums1[i] < nums2[j] -> {
+                union.add(nums1[i])
+                i++
+            }
+            nums1[i] > nums2[j] -> {
+                union.add(nums2[j])
+                j++
+            }
+            else -> {
+                union.add(nums1[i])
+                i++
+                j++
+            }
+        }
+    }
+
+    while (i < nums1.size){
+        union.add(nums1[i])
+        i++
+    }
+
+    while (j < nums2.size){
+        union.add(nums2[j])
+        j++
+    }
+
+    return union
+}
+
+fun main(){
+
+    val nums1 = listOf<Int>(1,3,5,7,9,12)
+    val nums2 = listOf<Int>(0,1,2,3,4,6,8,10,12)
+
+    val union = approach2(nums1, nums2)
+    println(union)  // Output: [1, 1, 2, 3, 3, 4]
+}
+
+
+/*
+
+// Homework 1
+
+fun mergeArrays(array1: List<Int>, array2: List<Int>): List<Int> {
+    val resultArray = mutableListOf<Int>()
+    var i = 0
+    var j = 0
+
+    while (i < array1.size && j < array2.size) {
+        if (array1[i] <= array2[j]) {
+            resultArray.add(array1[i])
+            i++
+        } else {
+            resultArray.add(array2[j])
+            j++
+        }
+    }
+
+    // Add the remaining elements of array1
+    while (i < array1.size) {
+        resultArray.add(array1[i])
+        i++
+    }
+
+    // Add the remaining elements of array2
+    while (j < array2.size) {
+        resultArray.add(array2[j])
+        j++
+    }
+
+    return resultArray
+}
+
+fun main() {
+    val array1 = listOf(1, 2, 3, 4, 5)
+    val array2 = listOf(1, 3, 4)
+    val resultArray = mergeArrays(array1, array2)
+    println(resultArray)  // Output: [1, 1, 2, 3, 3, 4]
+}
+
+
+
+
+
+
+// Homework2
+fun maxDistanceBetweenIdenticalChars(s: String): Int {
+    val charIndices = mutableMapOf<Char, Int>()
+    var maxDistance = -1
+
+    for (i in s.indices) {
+        val c = s[i]
+        if (charIndices.containsKey(c)) {
+            // Calculate the distance between current index and first occurrence of the character
+            val firstOccurrence = charIndices[c]!!
+            val distance = i - firstOccurrence - 1
+            maxDistance = maxOf(maxDistance, distance)
+        } else {
+            // Store the first occurrence of the character
+            charIndices[c] = i
+        }
+    }
+
+    return maxDistance
+}
+
+fun main() {
+    // Test cases
+    val s1 = "abbbabac"
+    val s2 = "abqcdefbghqi"
+    val s3 = "abcdef"
+
+    println(maxDistanceBetweenIdenticalChars(s1))  // Output: 5
+    println(maxDistanceBetweenIdenticalChars(s2))  // Output: 7
+    println(maxDistanceBetweenIdenticalChars(s3))  // Output: -1
+}
+
+*/
